86d0f7dd9c3a2225a2abc0f18a28c744a8e13c58
kemalreza12 <kemalreza92@gmail.com>
2021-03-23 10:56:44 +0700

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>test</h1>
+            </div>
+        )
+    }
+}
