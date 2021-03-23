9748c8709a97d7e843ed562c4c8874c61a49e7df
kemalreza12 <kemalreza92@gmail.com>
2021-03-23 23:37:10 +0700

    component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import Home from './pages/Home'
+import Navbar from './component/Navbar'
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
       </Switch>
